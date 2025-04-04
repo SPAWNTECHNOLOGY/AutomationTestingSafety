30e2a087fdc307efd9d3ecc7ad671b32c91756cb
PopovKirill <popovka@tik.perm.ru>
2025-04-05 01:21:21 +0500

    Добавьте файлы проекта.

---
+++ AutomationTestingSafety/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using AutomationTestingSafety.Database.Migration;
+
+namespace AutomationTestingSafety
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Замените строку подключения на свою
+            string connectionString = @"Data Source=DESKTOP-L61C90H;Initial Catalog=AutomationTestingSafety;Integrated Security=True;";
+
+            var migration = new MigrationRunner(connectionString);
+            try
+            {
+                migration.RunMigration();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Migration failed: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ AutomationTestingSafety/Database/Migration/MigrationRunner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTestingSafety.Database.Migration
+{
+    public class MigrationRunner
+    {
+        private readonly string _connectionString;
+
+        public MigrationRunner(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void RunMigration()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                // Создание таблицы Positions, если она не существует
+                var createPositionsTable = @"
+                    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Positions')
+                    BEGIN
+                        CREATE TABLE Positions
+                        (
+                            PositionID INT PRIMARY KEY IDENTITY(1,1),
+                            PositionName NVARCHAR(200) NOT NULL
+                        )
+                    END";
+                using (var command = new SqlCommand(createPositionsTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                // Создание таблицы Users, если она не существует
+                var createUsersTable = @"
+                    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Users')
+                    BEGIN
+                        CREATE TABLE Users
+                        (
+                            UserID INT PRIMARY KEY IDENTITY(1,1),
+                            FullName NVARCHAR(200) NOT NULL,
+                            Login NVARCHAR(100) NOT NULL,
+                            Password NVARCHAR(100) NOT NULL,
+                            RegistrationDate DATETIME NOT NULL DEFAULT GETDATE(),
+                            PositionID INT NOT NULL,
+                            CONSTRAINT FK_Users_Positions FOREIGN KEY (PositionID)
+                                REFERENCES Positions(PositionID)
+                        )
+                    END";
+                using (var command = new SqlCommand(createUsersTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
