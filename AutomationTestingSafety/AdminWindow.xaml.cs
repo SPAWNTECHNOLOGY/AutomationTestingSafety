2cfc49d390098ec34598da418eeb64fd78f96c90
PopovKirill <popovka@tik.perm.ru>
2025-04-05 02:54:24 +0500

    нейминг окон + кнопки выхода из профиля

--- AutomationTestingSafety/AdminWindow.xaml.cs
+++ AutomationTestingSafety/AdminWindow.xaml.cs
@@ -13,5 +13,12 @@ namespace AutomationTestingSafety
             _userInfo = userInfo;
             lblUserInfo.Content = $"Добро пожаловать, {_userInfo.FullName} (Администратор). Дата регистрации: {_userInfo.RegistrationDate:d}";
         }
+
+        private void ExitProfile(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new MainWindow();
+            loginWindow.Show();
+            this.Close();
+        }
     }
 }
--- AutomationTestingSafety/EmployeeWindow.xaml.cs
+++ AutomationTestingSafety/EmployeeWindow.xaml.cs
@@ -25,5 +25,12 @@ namespace AutomationTestingSafety
             _userInfo = userInfo;
             lblUserInfo.Content = $"Добро пожаловать, {_userInfo.FullName} (Сотрудник). Дата регистрации: {_userInfo.RegistrationDate:d}";
         }
+
+        private void ExitProfile(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new MainWindow();
+            loginWindow.Show();
+            this.Close();
+        }
     }
 }
